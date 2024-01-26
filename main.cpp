afcccb5cf078225cbe7456a7a758a8b48b4eeec4
rofrankly <ryfrnkl_nvrr@outlook.es>
2024-01-26 13:06:01 -0500

    se agrego el main base

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"\t\testa es nuestra primera colaboracion"<<endl;
+    //tenemos que crear dos clases una de persona y otra de mascota
+    //yo creare la mascota y tu la persona
+    // solo 2 atributos, y metodos mostrar, constructor,
+    //lo haces con el .h sin .cpp
+
+    return 0;
+}
